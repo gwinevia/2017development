8eef5dd710a577eadab36df6268e3c8d1f6f53ca
momoko karasawa <gretel.leez@gmail.com>
2017-01-25 17:33:15 +0900

    ページ移動＆servletからjspに移動もok

--- Java_programming/JSP_Servlet/tomcatTest/WEB-INF/src/tomcatTest/HelloWorld.java
+++ Java_programming/JSP_Servlet/tomcatTest/WEB-INF/src/tomcatTest/HelloWorld.java
@@ -2,19 +2,15 @@ package tomcatTest;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.util.*;
 
-public class HelloWorld extends HttpServlet{
- public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException{
-	 response.setContentType("text/html");
-	 PrintWriter out = response.getWriter();
-	 out.println("<html>");
-	 out.println("<head>");
-	 out.println("<title>HelloJavaServlet</title>");
-	 out.println("</head>");
-	 out.println("<body>");
-	 out.println("<h1>Hello World!!</h1>");
-	 out.println("</body>");
-	 out.println("</html>");
-	 out.close();
- }
+public class HelloWorld extends HttpServlet {
+	
+    public HelloWorld() {
+        super();
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	response.sendRedirect("/tomcat9/tomcatTest/HelloWorld.jsp");
+    }
 }

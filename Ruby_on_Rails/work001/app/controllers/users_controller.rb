b9f47e0d4db19d0d7b9ec3b5c17352bf43c479d4
momoko karasawa <gretel.leez@gmail.com>
2017-02-13 13:59:33 +0900

    up

--- Ruby_on_Rails/work001/app/controllers/users_controller.rb
+++ Ruby_on_Rails/work001/app/controllers/users_controller.rb
@@ -1,20 +1,37 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-    	log_in @user
-    	flash[:success] = "藤田研究室へようこそ!"
+      log_in @user
+      flash[:success] = "藤田研究室へようこそ!"
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "更新しました"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -23,5 +40,20 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end

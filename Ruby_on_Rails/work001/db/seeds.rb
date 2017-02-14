802e47ea95f648fd3cea076a578d3eb7f9de3765
momoko karasawa <gretel.leez@gmail.com>
2017-02-14 16:07:11 +0900

    seeds.rb up

--- Ruby_on_Rails/work001/db/seeds.rb
+++ Ruby_on_Rails/work001/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "SFlab",
+             email: "sflab@alpha.sf.cs.it-chiba.ac.jp",
+             password:              "enjoysf",
+             password_confirmation: "enjoysf",
+             admin: true)
+
+User.create!(name:  "Momoko Karasawa",
+             email: "gretel.leez@gmail.com",
+             password:              "grqt58yj",
+             password_confirmation: "grqt58yj",
+             admin: false)
